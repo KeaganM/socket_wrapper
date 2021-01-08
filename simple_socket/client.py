510b198b8e168583a31210980718e7af0901bd19
Keagan McNew <kmcnew2@gmail.com>
2021-01-07 23:39:38 -0600

    Initial Commit

--- simple_socket/client.py
+++ simple_socket/client.py
@@ -1,19 +1,63 @@
 import socket
 import select
+from dataclasses import dataclass
+from typing import Any, Union, ByteString
+import json
+
+
+class Message:
+    def __init__(self, content: str, proto_header_len: int = 10, **headers):
+        self.content = {'data': content}
+
+        self.headers = headers
+        self.headers.update({'content-length': len(json.dumps(self.content))})
+
+        self._proto_header_len = proto_header_len
+
+    def send(self):
+        # if type == 'header':
+        header = self._prep_to_send(self.headers)
+        message = self._prep_to_send(self.content)
+        return f'{len(header):<{self._proto_header_len}}{header}{message}'.encode('utf-8')
+        # else:
+        #     return self._prep_to_send(self.content).encode('utf-8')
+
+    def _prep_to_send(self, header_or_content: dict):
+        message = {key: value for key, value in header_or_content.items()}
+        return json.dumps(message)
+
+    def __repr__(self):
+        return f'{self.__dict__}'
+
 
 class Client:
-    def __init__(self,HOST='127.0.0.1',PORT=65000):
+    def __init__(self, HOST='127.0.0.1', PORT=65000):
         self._host = HOST
         self._port = PORT
 
-    def start_client(self):
-        client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        client_socket.connect((self._host,self._port))
+    def start_client(self, message):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((self._host, self._port))
         client_socket.setblocking(False)
 
-        client_socket.send(b'hello from the client')
+        # header
+        client_socket.send(message.send())
+        # # message
+        # client_socket.send(message.send('message'))
 
 
 if __name__ == '__main__':
+    m = 'hello from the client'
+
+    header = {
+        'content-type': 'text/json',
+        'content-encoding': 'utf-8',
+    }
+
+    message = Message(m, **header)
+    # r = message.send('header')
+    # print(r)
+
     c = Client()
-    pass
+    print('starting the client')
+    c.start_client(message)
--- simple_socket/host.py
+++ simple_socket/host.py
@@ -2,6 +2,59 @@ import socket
 import json
 import select
 
+from typing import Callable, Union
+
+import time
+
+
+class Receiver:
+    _header_length = None
+    _header = None
+
+    def __init__(self, proto_header_len: int = 10):
+        self._proto_header_length = proto_header_len
+        self.buffer = ''
+
+    def process(self, data) -> Union[None, str]:
+
+        self.buffer += data.decode('utf-8')
+
+        try:
+            if not self._header_length and len(self.buffer) >= self._proto_header_length:
+                self._process_decorator(self._process_proto_header, self._proto_header_length)
+
+            if not self._header and len(self.buffer) >= self._header_length:
+                self._process_decorator(self._process_header, self._header_length)
+
+            if len(self.buffer) >= int(self._header['content-length']):
+                msg = json.loads(self.buffer)
+                self._clean()
+                return msg
+        except TypeError:
+            pass
+        return None
+
+    def _process_decorator(self, process_f: Callable, start_index: int) -> None:
+        process_f()
+        self._truncate_buffer(start_index)
+
+    def _process_proto_header(self) -> None:
+        self._header_length = int(self.buffer[:self._proto_header_length].strip())
+
+    def _process_header(self) -> None:
+        self._header = json.loads(self.buffer[:self._header_length])
+
+    def _truncate_buffer(self, start_index: int) -> None:
+        self.buffer = self.buffer[start_index:]
+
+    def _clean(self) -> None:
+        self.buffer = ''
+        self._header_length = None
+        self._header = None
+
+    def __repr__(self):
+        return f'{self.__dict__}'
+
 
 class Server:
     _socket_list = []
@@ -12,7 +65,7 @@ class Server:
         self._port = PORT
 
     def start_server(self):
-        print('starting server')
+
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server_socket.bind((self._host, self._port))
@@ -21,15 +74,21 @@ class Server:
         self._socket_list.append(server_socket)
 
         while True:
-            read_sockets, _, exception_sockets = select.select(self._socket_list,[],self._socket_list)
+            read_sockets, _, exception_sockets = select.select(self._socket_list, [], self._socket_list)
 
             for notified_socket in read_sockets:
                 if notified_socket == server_socket:
-                    print(f'connected to client from {client_address[0]}:{client_address[1]}')
                     client_socket, client_address = server_socket.accept()
+                    print(f'connected to client from {client_address[0]}:{client_address[1]}')
 
-                    data = client_socket.recv(4000)
-                    print(data)
+                    data = client_socket.recv(1024)
+                    r = Receiver()
+                    res = r.process(data)
+
+                    while not res:
+                        data = client_socket.recv(1024)
+                        res = r.process(data)
+                    print(res)
 
 
 if __name__ == '__main__':
